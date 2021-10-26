80526897c5995f6afbe72d9640e2b7a6cd495483
darlison-github/Projetos <darlison-henrique@hotmail.com>
2021-10-26 10:00:24 -0300

    Estruturas Condicionais

--- OperadoresAritméticos/src/operadores/Gerador_de_Numeros.java
+++ OperadoresAritméticos/src/operadores/Gerador_de_Numeros.java
@@ -3,7 +3,7 @@ package operadores;
 public class Gerador_de_Numeros {
 
 	public static void main(String[] args) {
-		double aleatorio = Math.random(); // random é um gerador de números entre 0 e 1
+		double aleatorio = Math.random(); // random é um gerador de números aleatórios entre 0 e 1
 		int n = (int) (15 + aleatorio * (50-15));
 		System.out.println(n);
 		
