47c775ba99aa3e78d9b4fe0b4471dd866371dafe
darlison-github <darlison-henrique@hotmail.com>
2022-02-16 21:16:13 -0300

    ProjetinhoLivro (Herança)

--- OperadoresAritméticos/src/operadores/Arredondamentos.java
+++ OperadoresAritméticos/src/operadores/Arredondamentos.java
@@ -4,7 +4,7 @@ public class Arredondamentos {
 
 	public static void main(String[] args) {
 		float v = 8.5f;
-		int ar = (int) Math.ceil(v); // floor - arredonda para baixo, ceil - arredonda para cima
+		int ar = (int) Math.floor(v); // floor - arredonda para baixo, ceil - arredonda para cima
 		System.out.println(ar);
 		
 	}
--- OperadoresAritméticos/src/operadores/Incremento_Decremento.java
+++ OperadoresAritméticos/src/operadores/Incremento_Decremento.java
@@ -3,8 +3,8 @@ package operadores;
 public class Incremento_Decremento {
 
 	public static void main(String[] args) {
-		int n1 = 3;
-		int n2 = 5;
+		int n1 = 8;
+		int n2 = 8;
 		float m = (n1+n2)/2;
 		System.out.println("A média é igual a " + m);
 		
