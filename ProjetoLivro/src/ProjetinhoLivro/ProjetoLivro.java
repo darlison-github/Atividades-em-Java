56c4cbf9ace3864682ad2fa21c1f46bc229afe77
darlison-github <darlison-henrique@hotmail.com>
2022-02-16 21:24:49 -0300

    ProjetoLivro usando Herança

--- ProjetoLivro/src/ProjetinhoLivro/ProjetoLivro.java
+++ ProjetoLivro/src/ProjetinhoLivro/ProjetoLivro.java
@@ -3,10 +3,11 @@ public class ProjetoLivro {
 	public static void main(String[] args) {
 
 		Pessoa[] p = new Pessoa[3];
-		Livro[] l = new Livro[4];
+		Livro[] l = new Livro[3];
 		
 		p[0] = new Pessoa("Pedro", 26, "Masculino");
 		p[1] = new Pessoa("Ana", 20, "Feminina");
+		p[2] = new Pessoa("Wal", 56, "Feminina");
 		
 		l[0] = new Livro("Senhor dos Aneis", "Darlison Henrique", 1000, p[0]);
 		l[1] = new Livro("O Robbit", "Ana Luiza", 500, p[1]);
@@ -21,6 +22,11 @@ public class ProjetoLivro {
 		l[1].folhear(50);
 		l[1].avancarPag();
 		System.out.println(l[1].detalhes());
+		
+		l[2].abrir();
+		l[2].folhear(4);
+		l[2].avancarPag();
+		System.out.println(l[2].detalhes());
 		
 	}
 
